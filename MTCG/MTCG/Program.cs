3debf8b08a4c9184b2a845b5f4b9c895e82592c7
Yuni1204 <seunghyun2001@hotmail.com>
2022-12-12 15:26:07 +0100

    2

--- MTCG/MTCG/Program.cs
+++ MTCG/MTCG/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,16 @@ namespace MTCG
     {
         static void Main(string[] args)
         {
+            // socket from moodle
+            //var serverSock = new Socket(
+            //    AddressFamily.InterNetwork,
+            //    SocketType.Stream,
+            //    ProtocolType.Tcp
+            //);
+            //serverSock.Bind(new IPEndPoint(IPAddress.Loopback, 10101));
+            //serverSock.Listen(5);
+            //var clientSock = serverSock.Accept();
+            //clientSock.Send(Encoding.ASCII.GetBytes("Hello World"));
             var Game = new Game();
             Game.Beschimpfen();
             return;
